d60611c78ee3db5d84764c5696a7a1774915f1c3
Nizar YASSINE <yassiniz@inf-48-tst>
2016-03-25 14:04:16 +0100

    producer premiere version

---
+++ TP5/univ/src/main/java/fr/eip/ProducerConsumer.java
@@ -0,0 +1,21 @@
+package fr.eip;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.log4j.BasicConfigurator;
+
+/**
+ * Created by yassiniz on 25/03/16.
+ */
+public class ProducerConsumer {
+
+    public static void main(String[] args) throws Exception{
+        //Configure le logger par défaut
+        BasicConfigurator.configure();
+
+        //Contexte Camel par défaut
+
+        }
+}
